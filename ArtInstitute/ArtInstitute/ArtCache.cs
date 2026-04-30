e4da3272af00da96e40d2a096fdde0b8817a57aa
Danilo Denic <danilo.denic@elfak.rs>
2026-04-30 16:46:42 +0200

    Created folder and added Cache

---
+++ ArtInstitute/ArtInstitute/ArtCache.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+namespace SistemskoProjekat;
+
+public class ArtCache
+{
+    private readonly int maxSize;
+    private readonly Dictionary<string,string> data = new();
+    private readonly LinkedList<string> lruList = new();
+    private readonly object lockobj = new();
+
+    public ArtCache(int maxSize)
+    {
+        this.maxSize = maxSize;
+    }
+
+    public bool TryGet(string key, out string value)
+    {
+        lock (lockobj)
+        {
+            if(data.TryGetValue(key,out value))
+            {
+                //Pomeramo na pocetak liste jer je recently used
+                lruList.Remove(key); 
+                lruList.AddFirst(key);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public void Add(string key, string value)
+    {
+        lock (lockobj)
+        {
+            if (!data.ContainsKey(key))
+            {
+                if(data.Count >= maxSize)
+                {
+                    //Izbacujemo najstarijeg tj poslednjeg u listi
+                    var najstariji = lruList.Last.Value; 
+                    data.Remove(najstariji);
+                    lruList.RemoveLast();
+                }
+                data.Add(key,value);
+            }
+            else
+            {
+                lruList.Remove(key);
+            }
+            lruList.AddFirst(key);
+        }
+    }
+}
---
+++ ArtInstitute/ArtInstitute/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
